02d5671212aae202224ccef8114cedbd89ff6275
noahBarreau <n.barreau17@gmail.com>
2024-11-25 11:55:16 +0100

    les images changent en fonction tour

--- js/gestionCase.js
+++ js/gestionCase.js
@@ -1,5 +1,16 @@
 function changeImg(id, turn)
 {
-    //document.getElementById(id).style.visibility="hidden";
-    document.getElementById(id).className="fa-regular fa-circle";
+    forme=document.getElementById(id).getAttribute("forme");
+    circle="fa-regular fa-circle";
+    cross="fa-solid fa-x";
+
+    if(forme==""){
+        if(turn==0){
+            document.getElementById(id).className=circle;
+            document.getElementById(id).setAttribute("forme", "circle");
+        }else{
+            document.getElementById(id).className=cross;
+            document.getElementById(id).setAttribute("forme","cross");
+        }
+    }
 }
